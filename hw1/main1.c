1ac82ced1f9c6de8b08e8f0a577bbbb287ef6070
Prince69 <151009341+Prince0909070@users.noreply.github.com>
2025-03-05 22:23:52 +0800

    Update main1.c

--- hw1/main1.c
+++ hw1/main1.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+void runLengthEncoding(char *str) {
+    int len = strlen(str);
+    for (int i = 0; i < len; i++) {
+        char current = str[i];
+        int count = 1;
+        
+        while (i + 1 < len && str[i + 1] == current) {
+            count++;
+            i++;
+        }
+        
+        printf("%c%d", current, count);
+    }
+    printf("\n");
+}
+
+int main() {
+    char ary[] = "AABBBCCCCddd";
+    printf("輸入: %s\n", ary);
+    printf("輸出: ");
+    runLengthEncoding(ary);
+    return 0;
+}
 
